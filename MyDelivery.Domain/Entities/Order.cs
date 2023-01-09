76c57e6de1667c2c4b1f296fb34e85384ceffc63
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-01-09 13:54:39 -0300

    feat: cria a entidade order

---
+++ MyDelivery.Domain/Entities/Order.cs
@@ -0,0 +1,37 @@
+﻿using MyDelivery.Domain.Validations;
+namespace MyDelivery.Domain.Entities;
+
+public class Order
+{
+    public int Id { get; private set; }
+    public int PersonId { get; private set; }
+    public int ProductId { get; private set; }
+    public DateTime Date { get; private set; }
+
+    public Person Person { get; set; }
+    public Product Product { get; set; }
+
+    public Order(int productId, int personId, DateTime date)
+    {
+        Validate(productId, personId, date);
+    }
+
+    public Order(int id, int productId, int personId, DateTime date)
+    {
+        DomainValidationException.If(id < 0, "O Id deve ser maior que zero");
+        Id = id;
+        Validate(productId, personId, date);
+    }
+
+    private void Validate(int productId, int personId, DateTime? date)
+    {
+        DomainValidationException.If(productId < 0, "O Produto é inválido");
+        DomainValidationException.If(personId < 0, "A Pessoa é inválida");
+        DomainValidationException.If(!date.HasValue, "A Data é inválida");
+
+        PersonId = personId;
+        ProductId = productId;
+        Date = date.Value;
+    }
+}
+
