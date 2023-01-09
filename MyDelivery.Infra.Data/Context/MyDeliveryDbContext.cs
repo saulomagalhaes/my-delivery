be51d0adda9b8ebfb503d283635f6fa14c43fa5f
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-01-09 16:39:54 -0300

    feat: cria conexao db com a tabela people

---
+++ MyDelivery.Infra.Data/Context/MyDeliveryDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyDelivery.Domain.Entities;
+
+namespace MyDelivery.Infra.Data.Context;
+
+public class MyDeliveryDbContext : DbContext
+{
+	public MyDeliveryDbContext(DbContextOptions<MyDeliveryDbContext> options): base(options){}
+
+    public DbSet<Person> People { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(MyDeliveryDbContext).Assembly);
+    }
+}
---
+++ MyDelivery.Infra.Data/Maps/PersonMap.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyDelivery.Domain.Entities;
+namespace MyDelivery.Infra.Data.Maps;
+
+public class PersonMap : IEntityTypeConfiguration<Person>
+{
+    public void Configure(EntityTypeBuilder<Person> builder)
+    {
+        builder.ToTable("people");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("id")
+            .UseIdentityColumn();
+
+        builder.Property(x => x.Name)
+            .HasColumnName("name");
+
+        builder.Property(x => x.Document)
+            .HasColumnName("document");
+
+        builder.Property(x => x.Phone)
+            .HasColumnName("phone");
+
+        builder.HasMany(x => x.Purchases)
+            .WithOne(x => x.Person)
+            .HasForeignKey(x => x.PersonId);
+    }
+}
