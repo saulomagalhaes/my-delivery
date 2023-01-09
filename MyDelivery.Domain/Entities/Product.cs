a2d1eb2c7ac07d4e1365ee253a5aca5bc4636d98
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-01-09 13:54:23 -0300

    feat: cria a entidade product

---
+++ MyDelivery.Domain/Entities/Product.cs
@@ -0,0 +1,36 @@
+﻿using MyDelivery.Domain.Validations;
+namespace MyDelivery.Domain.Entities;
+
+public class Product
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public string Code { get; private set; }
+    public decimal Price { get; private set; }
+
+    public ICollection<Order> Orders { get; set; }
+
+
+    public Product(string name, string code, decimal price)
+    {
+        Validate(name, code, price);
+    }
+
+    public Product(int id, string name, string code, decimal price)
+    {
+        DomainValidationException.If(id < 0, "O Id deve ser maior que zero");
+        Id = id;
+        Validate(name, code, price);
+    }
+
+    private void Validate(string name, string code, decimal price)
+    {
+        DomainValidationException.If(String.IsNullOrWhiteSpace(name), "O Nome é inválido");
+        DomainValidationException.If(String.IsNullOrWhiteSpace(code), "O Código é inválido");
+        DomainValidationException.If(price < 0, "O Preço é inválido");
+
+        Name = name;
+        Code = code;
+        Price = price;
+    }
+}
