9f10a08e400b3d023605c94a06ab674d740ee734
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-01-09 11:57:22 -0300

    feat: cria a entidade person

---
+++ MyDelivery.Domain/Entities/Person.cs
@@ -0,0 +1,33 @@
+﻿using MyDelivery.Domain.Validations;
+namespace MyDelivery.Domain.Entities;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Document { get; set; }
+    public string Phone { get; set; }
+
+    public Person(string name, string document, string phone)
+    {
+        Validate(name, document, phone);
+    }
+
+    public Person (int id, string name, string document, string phone)
+    {
+        DomainValidationException.If(id < 0, "O Id deve ser maior que zero");
+        Id = id;
+        Validate(name, document, phone);
+    }
+
+    private void Validate(string name, string document, string phone)
+    {
+        DomainValidationException.If(String.IsNullOrWhiteSpace(name), "O Nome é inválido");
+        DomainValidationException.If(String.IsNullOrWhiteSpace(document), "O Nome é inválido");
+        DomainValidationException.If(String.IsNullOrWhiteSpace(phone), "O Nome é inválido");
+
+        Name= name;
+        Document= document;
+        Phone= phone;
+    }
+}
