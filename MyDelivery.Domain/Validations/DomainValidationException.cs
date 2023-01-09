5d8c47fd51ba6200b99fb7c1ee91e044cc66592a
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-01-09 11:58:56 -0300

    feat: cria uma exception para a validacao do dominio

---
+++ MyDelivery.Domain/Validations/DomainValidationException.cs
@@ -0,0 +1,11 @@
+﻿namespace MyDelivery.Domain.Validations;
+
+public class DomainValidationException : Exception
+{
+	public DomainValidationException(string error): base(error) { }
+
+	public static void If(bool hasError, string message)
+	{
+		if(hasError) throw new DomainValidationException(message);
+	}
+}
