340a4e426ad58cfcbe41c58445ffb0774a3e0b62
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-01-09 17:01:56 -0300

    feat: cria productmap e purchasemap

---
+++ MyDelivery.Infra.Data/Maps/ProductMap.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyDelivery.Domain.Entities;
+namespace MyDelivery.Infra.Data.Maps;
+
+public class ProductMap : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("products");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("id")
+            .UseIdentityColumn();
+
+        builder.Property(x => x.Name)
+            .HasColumnName("name");
+
+        builder.Property(x => x.Code)
+            .HasColumnName("code");
+
+        builder.Property(x => x.Price)
+            .HasColumnName("price");
+
+        builder.HasMany(x => x.Purchases)
+            .WithOne(x => x.Product)
+            .HasForeignKey(x => x.ProductId);
+    }
+}
---
+++ MyDelivery.Infra.Data/Maps/PurchasesMap.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyDelivery.Domain.Entities;
+namespace MyDelivery.Infra.Data.Maps;
+
+public class PurchasesMap : IEntityTypeConfiguration<Purchase>
+{
+    public void Configure(EntityTypeBuilder<Purchase> builder)
+    {
+        builder.ToTable("purchases");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("id")
+            .UseIdentityColumn();
+
+        builder.Property(x => x.PersonId)
+            .HasColumnName("person_id");
+
+        builder.Property(x => x.ProductId)
+            .HasColumnName("product_id");
+
+        builder.Property(x => x.Date)
+            .HasColumnName("date");
+
+        builder.HasOne(x => x.Person)
+            .WithMany(x => x.Purchases);
+
+        builder.HasOne(x => x.Product)
+            .WithMany(x => x.Purchases);
+    }
+}
