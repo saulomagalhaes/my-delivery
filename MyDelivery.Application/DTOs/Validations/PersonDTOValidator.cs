c12e3684a54e79fd79e07eb3f91ac7f04da18537
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-01-10 19:29:13 -0300

    feat: cria PersonDTO e validacao

---
+++ MyDelivery.Application/DTOs/Validations/PersonDTOValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace MyDelivery.Application.DTOs.Validations;
+
+public class PersonDTOValidator : AbstractValidator<PersonDTO>
+{
+    public PersonDTOValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("O nome deve ser informado");
+
+        RuleFor(x => x.Document)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("O documento deve ser informado");
+
+        RuleFor(x => x.Phone)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("O telefone deve ser informado");
+    }
+}
