2f069497061e6f412194cd109b563054ca17da11
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-01-11 20:22:20 -0300

    feat: adiciona opcao para ignorar nulos no retorno da requisicao

--- MyDelivery.Api/Program.cs
+++ MyDelivery.Api/Program.cs
@@ -1,10 +1,12 @@
 using MyDelivery.Infra.IoC;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddInfra(builder.Configuration);
 builder.Services.AddServices(builder.Configuration);
+builder.Services.AddMvc().AddJsonOptions(options => { options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull; });
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
