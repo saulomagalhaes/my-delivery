76a582cffd146174f713036cbf6e350f058c9c0c
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-01-11 13:19:38 -0300

    feat(person): adiciona um profile para mapeamento

---
+++ MyDelivery.Application/Profiles/PersonProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MyDelivery.Application.DTOs;
+using MyDelivery.Domain.Entities;
+
+namespace MyDelivery.Application.Profiles;
+
+public class PersonProfile : Profile
+{
+	public PersonProfile()
+	{
+		CreateMap<Person, PersonDTO>();
+		CreateMap<PersonDTO, Person>();
+
+	}
+}
