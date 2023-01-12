eec05de23ffae64d0266b7ea1f008ddd2733836e
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-01-12 12:18:29 -0300

    feat(product): cria dtos

---
+++ MyDelivery.Application/DTOs/Product/ProductDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MyDelivery.Application.DTOs.Product;
+
+public class ProductDTO
+{
+    public string Name { get; set; }
+    public string Code { get; set; }
+    public decimal Price { get; set; }
+}
---
+++ MyDelivery.Application/DTOs/Product/ReadProductDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MyDelivery.Application.DTOs.Product;
+
+public class ReadProductDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Code { get; set; }
+    public decimal Price { get; set; }
+}
