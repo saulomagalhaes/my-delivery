ffe6808c1ded580913e8bdbd507d609960e5e1bb
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-01-12 12:18:52 -0300

    feat(product): cria validacao com fluent

---
+++ MyDelivery.Application/DTOs/Validations/ProductDTOValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using MyDelivery.Application.DTOs.Product;
+
+namespace MyDelivery.Application.DTOs.Validations;
+
+public class ProductDTOValidator : AbstractValidator<ProductDTO>
+{
+    public ProductDTOValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("O nome deve ser informado");
+
+        RuleFor(x => x.Code)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("O código deve ser informado");
+
+        RuleFor(x => x.Price)
+            .NotEmpty()
+            .GreaterThan(0)
+            .WithMessage("O preço deve ser maior que 0");
+    }
+}
---
+++ MyDelivery.Application/Profiles/ProductProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using MyDelivery.Application.DTOs.Product;
+using MyDelivery.Domain.Entities;
+
+namespace MyDelivery.Application.Profiles;
+
+public class ProductProfile : Profile
+{
+	public ProductProfile()
+	{
+		CreateMap<Product, ProductDTO>();
+		CreateMap<ProductDTO, Product>();
+		CreateMap<Product, ReadProductDTO>();
+		CreateMap<ReadProductDTO, Product>();
+    }
+}
