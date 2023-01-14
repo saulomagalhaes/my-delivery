0f472da37630913ca56fa20cd6dc9281ecbfaae2
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-01-14 16:21:16 -0300

    feat(purchase): cria DTOS

---
+++ MyDelivery.Application/DTOs/Purchase/PurchaseDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace MyDelivery.Application.DTOs.Purchase;
+
+public class PurchaseDTO
+{
+    public string Code { get; set; }
+    public string Document { get; set; }
+}
---
+++ MyDelivery.Application/DTOs/Purchase/ReadPurchaseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MyDelivery.Application.DTOs.Purchase;
+
+public class ReadPurchaseDTO
+{
+    public int Id { get; set; }
+    public string Code { get; set; }
+    public string Document { get; set; }
+}
