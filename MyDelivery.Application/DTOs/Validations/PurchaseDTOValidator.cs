5969399a3c51bea2159341f3c2e9aba2b761aa1d
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-01-14 16:23:20 -0300

    feat(purchase): cria validacao pro purchase

---
+++ MyDelivery.Application/DTOs/Validations/PurchaseDTOValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using MyDelivery.Application.DTOs.Purchase;
+
+namespace MyDelivery.Application.DTOs.Validations;
+
+public class PurchaseDTOValidator : AbstractValidator<PurchaseDTO>
+{
+	public PurchaseDTOValidator()
+	{
+		RuleFor(x => x.Code)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("O codigo deve ser informado");
+
+        RuleFor(x => x.Document)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("O codigo deve ser informado");
+    }
+}
