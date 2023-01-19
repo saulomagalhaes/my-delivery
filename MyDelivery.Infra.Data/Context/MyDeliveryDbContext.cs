e25ee92631531117ad6f6861fd046736e3ef175d
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-01-19 10:22:12 -0300

    feat: adiciona tabela users ao contexto

--- MyDelivery.Infra.Data/Context/MyDeliveryDbContext.cs
+++ MyDelivery.Infra.Data/Context/MyDeliveryDbContext.cs
@@ -10,6 +10,7 @@ public class MyDeliveryDbContext : DbContext
     public DbSet<Person> People { get; set; }
     public DbSet<Product> Products { get; set; }
     public DbSet<Purchase> Purchases { get; set; }
+    public DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ MyDelivery.Infra.Data/Maps/UserMap.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyDelivery.Domain.Entities;
+
+namespace MyDelivery.Infra.Data.Maps;
+
+public class UserMap : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("users");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("id")
+            .UseIdentityColumn();
+
+        builder.Property(x => x.Email)
+            .HasColumnName("email");
+
+        builder.Property(x => x.Password)
+            .HasColumnName("password");
+    }
+}
